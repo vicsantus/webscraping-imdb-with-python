c36688c2e31e0d7e3c27c66bd197708c698fc39c
victrosantos <victor.santos.fk@hotmail.com>
2023-07-08 14:46:36 -0300

    feat: Consertei espaços duplos e \n no titulo

--- rottenScraping/rottenScraping/spiders/rottentomatoes.py
+++ rottenScraping/rottenScraping/spiders/rottentomatoes.py
@@ -1,5 +1,5 @@
 import scrapy
-import re
+# import re
 
 
 class RottentomatoesSpider(scrapy.Spider):
@@ -9,15 +9,14 @@ class RottentomatoesSpider(scrapy.Spider):
         "https://www.rottentomatoes.com/browse/movies_in_theaters/critics:certified_fresh"]
 
     def format(self, arg):  # Formata as strings retirando espaço duplo e \n
-        if isinstance(arg, list):
-            return [re.sub(r'\s{2,}|\n', ' ', film) for film in arg]
-        return re.sub(r'^(?!(.*\s\s|\n.*))(?!.*\s$)(?!.*\n$).*$', ' ', arg.strip())
+        newArg = arg.replace('\n', '').replace('  ', ' ').strip()
+        return newArg
 
     def parse(self, response):
         for films in response.css('.js-tile-link'):
             tituloUnformated = films.css('.p--small::text').get()
             yield {
-                "titulo": format(tituloUnformated),
+                "titulo": self.format(tituloUnformated),
                 "tomatometer": films.css(
                     '.js-tile-link score-pairs::attr(audiencescore)').get(),
                 "audience_score": films.css(
